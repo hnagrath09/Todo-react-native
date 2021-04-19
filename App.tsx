941c884c257227bad389bfa069df13473b4d4b37
Himanshu Nagrath <him.nagrath@gmail.com>
2021-04-19 08:43:00 +0530

    Base setup and task component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import Task from "./components/task";
+
+MIcon.loadFont();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          <Task text="Task 1" />
+          <Task text="Task 2" />
+          <Task text="Task 3" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +26,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED"
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20
+  },
+  items: {}
 });
